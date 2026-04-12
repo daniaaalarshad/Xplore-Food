47fbeb9188e7abd951cccf4384e258f0da144299
Danial <danial@stoel.no>
2026-04-12 14:08:33 +0200

    Stop tracking .next/ build folder

--- .next/dev/static/chunks/app_restaurants_[id]_page_tsx_2ed05fc5._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_110b1218._.js",
-  "static/chunks/node_modules_e4995192._.js"
-],
-    source: "dynamic"
-});
